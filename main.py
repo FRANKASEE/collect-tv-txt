66c30babfee24660623d00df58a874523a7f3ac7
FRANKASEE <149130864+FRANKASEE@users.noreply.github.com>
2024-08-30 16:29:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import urllib.request
 from urllib.parse import urlparse
-import re #正则
+import re  # 正则
 import os
 from datetime import datetime
 
 # 执行开始时间
 timestart = datetime.now()
-# 报时  '',
-#print(f"time: {datetime.now().strftime("%Y%m%d_%H_%M_%S")}")
+# 报时
+print(f"time: {datetime.now().strftime('%Y%m%d_%H_%M_%S')}")
 
-#读取文本方法
+# 读取文本方法
 def read_txt_to_array(file_name):
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
@@ -23,7 +23,7 @@ def read_txt_to_array(file_name):
         print(f"An error occurred: {e}")
         return []
 
-#read BlackList 2024-06-17 15:02
+# read BlackList 2024-06-17 15:02
 def read_blacklist_from_txt(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
@@ -31,23 +31,20 @@ def read_blacklist_from_txt(file_path):
     BlackList = [line.split(',')[1].strip() for line in lines if ',' in line]
     return BlackList
 
-blacklist_auto=read_blacklist_from_txt('blacklist/blacklist_auto.txt') 
-blacklist_manual=read_blacklist_from_txt('blacklist/blacklist_manual.txt') 
-# combined_blacklist = list(set(blacklist_auto + blacklist_manual))
-combined_blacklist = set(blacklist_auto + blacklist_manual)  #list是个列表，set是个集合，据说检索速度集合要快很多。2024-08-08
+blacklist_auto = read_blacklist_from_txt('blacklist/blacklist_auto.txt')
+blacklist_manual = read_blacklist_from_txt('blacklist/blacklist_manual.txt')
+combined_blacklist = set(blacklist_auto + blacklist_manual)
 
 # 定义多个对象用于存储不同内容的行文本
 sh_lines = []
-ys_lines = [] #CCTV
-ws_lines = [] #卫视频道
-ty_lines = [] #体育频道
+ys_lines = []  # CCTV
+ws_lines = []  # 卫视频道
+ty_lines = []  # 体育频道
 dy_lines = []
-gat_lines = [] #港澳台
-
-hn_lines = [] #地方台-湖南频道
-
-# favorite_lines = []
-
+dsj_lines = []
+gat_lines = []  # 港澳台
+gj_lines = []  # 国际台
+jlp_lines = []  # 纪录片
 other_lines = []
 
 def process_name_string(input_str):
@@ -60,164 +57,133 @@ def process_name_string(input_str):
     return result_str
 
 def process_part(part_str):
-    # 处理逻辑
-    if "CCTV" in part_str  and "://" not in part_str:
-        part_str=part_str.replace("IPV6", "")  #先剔除IPV6字样
-        part_str=part_str.replace("PLUS", "+")  #替换PLUS
-        part_str=part_str.replace("1080", "")  #替换1080
+    if "CCTV" in part_str and "://" not in part_str:
+        part_str = part_str.replace("IPV6", "")  # 先剔除IPV6字样
+        part_str = part_str.replace("PLUS", "+")  # 替换PLUS
+        part_str = part_str.replace("1080", "")  # 替换1080
         filtered_str = ''.join(char for char in part_str if char.isdigit() or char == 'K' or char == '+')
-        if not filtered_str.strip(): #处理特殊情况，如果发现没有找到频道数字返回原名称
-            filtered_str=part_str.replace("CCTV", "")
+        if not filtered_str.strip():  # 处理特殊情况，如果发现没有找到频道数字返回原名称
+            filtered_str = part_str.replace("CCTV", "")
 
-        if len(filtered_str) > 2 and re.search(r'4K|8K', filtered_str):   # 特殊处理CCTV中部分4K和8K名称
-            # 使用正则表达式替换，删除4K或8K后面的字符，并且保留4K或8K
+        if len(filtered_str) > 2 and re.search(r'4K|8K', filtered_str):  # 特殊处理CCTV中部分4K和8K名称
             filtered_str = re.sub(r'(4K|8K).*', r'\1', filtered_str)
-            if len(filtered_str) > 2: 
-                # 给4K或8K添加括号
+            if len(filtered_str) > 2:
                 filtered_str = re.sub(r'(4K|8K)', r'(\1)', filtered_str)
 
-        return "CCTV"+filtered_str 
-        
+        return "CCTV" + filtered_str
+
     elif "卫视" in part_str:
-        # 定义正则表达式模式，匹配“卫视”后面的内容
         pattern = r'卫视「.*」'
-        # 使用sub函数替换匹配的内容为空字符串
         result_str = re.sub(pattern, '卫视', part_str)
         return result_str
-    
+
     return part_str
 
 # 准备支持m3u格式
 def get_url_file_extension(url):
-    # 解析URL
     parsed_url = urlparse(url)
-    # 获取路径部分
     path = parsed_url.path
-    # 提取文件扩展名
     extension = os.path.splitext(path)[1]
     return extension
 
 def convert_m3u_to_txt(m3u_content):
-    # 分行处理
     lines = m3u_content.split('\n')
-    
-    # 用于存储结果的列表
     txt_lines = []
-    
-    # 临时变量用于存储频道名称
     channel_name = ""
-    
+
     for line in lines:
-        # 过滤掉 #EXTM3U 开头的行
         if line.startswith("#EXTM3U"):
             continue
-        # 处理 #EXTINF 开头的行
         if line.startswith("#EXTINF"):
-            # 获取频道名称（假设频道名称在引号后）
             channel_name = line.split(',')[-1].strip()
-        # 处理 URL 行
-        elif line.startswith("http") or line.startswith("rtmp") or line.startswith("p3p") :
+        elif line.startswith("http") or line.startswith("rtmp") or line.startswith("p3p"):
             txt_lines.append(f"{channel_name},{line.strip()}")
-    
-    # 将结果合并成一个字符串，以换行符分隔
+
     return '\n'.join(txt_lines)
 
-# 在list是否已经存在url 2024-07-22 11:18
 def check_url_existence(data_list, url):
-    """
-    Check if a given URL exists in a list of data.
-
-    :param data_list: List of strings containing the data
-    :param url: The URL to check for existence
-    :return: True if the URL exists in the list, otherwise False
-    """
-    # Extract URLs from the data list
     urls = [item.split(',')[1] for item in data_list]
-    return url not in urls #如果不存在则返回true，需要
+    return url not in urls
 
-# 处理带$的URL，把$之后的内容都去掉（包括$也去掉） 【2024-08-08 22:29:11】
 def clean_url(url):
-    last_dollar_index = url.rfind('$')  # 安全起见找最后一个$处理
+    last_dollar_index = url.rfind('$')
     if last_dollar_index != -1:
         return url[:last_dollar_index]
     return url
 
-# 分发直播源，归类，把这部分从process_url剥离出来，为以后加入whitelist源清单做准备。
 def process_channel_line(line):
-    if  "#genre#" not in line and "," in line and "://" in line:
-        channel_name=line.split(',')[0].strip()
-        channel_address=clean_url(line.split(',')[1].strip())  #把URL中$之后的内容都去掉
-        line=channel_name+","+channel_address #重新组织line
+    if "#genre#" not in line and "," in line and "://" in line:
+        channel_name = line.split(',')[0].strip()
+        channel_address = clean_url(line.split(',')[1].strip())
+        line = channel_name + "," + channel_address
 
-        if channel_address not in combined_blacklist: # 判断当前源是否在blacklist中
-            # 根据行内容判断存入哪个对象，开始分发
-            if "CCTV" in channel_name and check_url_existence(ys_lines, channel_address) : #央视频道
+        if channel_address not in combined_blacklist:
+            if "CCTV" in channel_name and check_url_existence(ys_lines, channel_address):
                 ys_lines.append(process_name_string(line.strip()))
-            elif channel_name in ws_dictionary and check_url_existence(ws_lines, channel_address): #卫视频道
+            elif channel_name in ws_lines and check_url_existence(ws_lines, channel_address):
                 ws_lines.append(process_name_string(line.strip()))
-            elif channel_name in ty_dictionary and check_url_existence(ty_lines, channel_address):  #体育频道
+            elif channel_name in ty_lines and check_url_existence(ty_lines, channel_address):
                 ty_lines.append(process_name_string(line.strip()))
-            elif channel_name in dy_dictionary and check_url_existence(dy_lines, channel_address):  #电影频道
+            elif channel_name in dy_lines and check_url_existence(dy_lines, channel_address):
                 dy_lines.append(process_name_string(line.strip()))
-            elif channel_name in gat_dictionary and check_url_existence(gat_lines, channel_address):  #港澳台
+            elif channel_name in dsj_lines and check_url_existence(dsj_lines, channel_address):
+                dsj_lines.append(process_name_string(line.strip()))
+            elif channel_name in sh_lines and check_url_existence(sh_lines, channel_address):
+                sh_lines.append(process_name_string(line.strip()))
+            elif channel_name in gat_lines and check_url_existence(gat_lines, channel_address):
                 gat_lines.append(process_name_string(line.strip()))
-            elif channel_name in hn_dictionary and check_url_existence(hn_lines, channel_address):  #地方台-湖南频道
-                hn_lines.append(process_name_string(line.strip()))
+            elif channel_name in gj_lines and check_url_existence(gj_lines, channel_address):
+                gj_lines.append(process_name_string(line.strip()))
+            elif channel_name in jlp_lines and check_url_existence(jlp_lines, channel_address):
+                jlp_lines.append(process_name_string(line.strip()))
             else:
                 other_lines.append(line.strip())
 
-
 def process_url(url):
     try:
-        other_lines.append("◆◆◆　"+url)  # 存入other_lines便于check 2024-08-02 10:41
-        # 打开URL并读取内容
+        other_lines.append("◆◆◆　" + url)
         with urllib.request.urlopen(url) as response:
-            # 以二进制方式读取数据
             data = response.read()
-            # 将二进制数据解码为字符串
             text = data.decode('utf-8')
-            # channel_name=""
-            # channel_address=""
 
-            #处理m3u和m3u8，提取channel_name和channel_address
-            if get_url_file_extension(url)==".m3u" or get_url_file_extension(url)==".m3u8":
-                text=convert_m3u_to_txt(text)
+            if get_url_file_extension(url) == ".m3u" or get_url_file_extension(url) == ".m3u8":
+                text = convert_m3u_to_txt(text)
 
-            # 逐行处理内容
             lines = text.split('\n')
             print(f"行数: {len(lines)}")
             for line in lines:
-                if  "#genre#" not in line and "," in line and "://" in line:
-                    # 拆分成频道名和URL部分
+                if "#genre#" not in line and "," in line and "://" in line:
                     channel_name, channel_address = line.split(',', 1)
-                    #需要加处理带#号源=予加速源
                     if "#" not in channel_address:
-                        process_channel_line(line) # 如果没有井号，则照常按照每行规则进行分发
-                    else: 
-                        # 如果有“#”号，则根据“#”号分隔
+                        process_channel_line(line)
+                    else:
                         url_list = channel_address.split('#')
                         for channel_url in url_list:
-                            newline=f'{channel_name},{channel_url}'
+                            newline = f'{channel_name},{channel_url}'
                             process_channel_line(newline)
 
-            other_lines.append('\n') #每个url处理完成后，在other_lines加个回车 2024-08-02 10:46
+            other_lines.append('\n')
 
     except Exception as e:
         print(f"处理URL时发生错误：{e}")
 
+current_directory = os.getcwd()
 
-current_directory = os.getcwd()  #准备读取txt
+# 读取文本
+ys_dictionary = read_txt_to_array('主频道/CCTV.txt')
+sh_dictionary = read_txt_to_array('主频道/shanghai.txt')
+ws_dictionary = read_txt_to_array('主频道/卫视频道.txt')
+ty_dictionary = read_txt_to_array('主频道/体育频道.txt')
+dy_dictionary = read_txt_to_array('主频道/电影.txt')
+dsj_dictionary = read_txt_to_array('主频道/电视剧.txt')
+gat_dictionary = read_txt_to_array('主频道/港澳台.txt')
+gj_dictionary = read_txt_to_array('主频道/国际台.txt')
+jlp_dictionary = read_txt_to_array('主频道/纪录片.txt')
 
-#读取文本
-ys_dictionary=read_txt_to_array('主频道/CCTV.txt') #仅排序用
-ws_dictionary=read_txt_to_array('主频道/卫视频道.txt') #过滤+排序
-ty_dictionary=read_txt_to_array('主频道/体育频道.txt') #过滤
-dy_dictionary=read_txt_to_array('主频道/电影.txt') #过滤
-gat_dictionary=read_txt_to_array('主频道/港澳台.txt') #过滤
+# 定义
+urls = read_txt_to_array('assets/urls-daily.txt')
 
-hn_dictionary=read_txt_to_array('地方台/湖南频道.txt') #过滤
-
-#读取纠错频道名称方法
+# 读取纠错频道名称方法
 def load_corrections_name(filename):
     corrections = {}
     with open(filename, 'r', encoding='utf-8') as f:
@@ -228,11 +194,10 @@ def load_corrections_name(filename):
                 corrections[name] = correct_name
     return corrections
 
-#读取纠错文件
+# 读取纠错文件
 corrections_name = load_corrections_name('assets/corrections_name.txt')
 
-#纠错频道名称
-#correct_name_data(corrections_name,xxxx)
+# 纠错频道名称
 def correct_name_data(corrections, data):
     corrected_data = []
     for line in data:
@@ -242,78 +207,38 @@ def correct_name_data(corrections, data):
         corrected_data.append(f"{name},{url}")
     return corrected_data
 
-
-
 def sort_data(order, data):
-    # 创建一个字典来存储每行数据的索引
     order_dict = {name: i for i, name in enumerate(order)}
     
-    # 定义一个排序键函数，处理不在 order_dict 中的字符串
     def sort_key(line):
         name = line.split(',')[0]
         return order_dict.get(name, len(order))
     
-    # 按照 order 中的顺序对数据进行排序
     sorted_data = sorted(data, key=sort_key)
     return sorted_data
 
-
-
-
-
-# 定义
-urls = read_txt_to_array('assets/urls-daily.txt')
 # 处理
 for url in urls:
     print(f"处理URL: {url}")
     process_url(url)
 
-
-
-# 定义一个函数，提取每行中逗号前面的数字部分作为排序的依据
-def extract_number(s):
-    num_str = s.split(',')[0].split('-')[1]  # 提取逗号前面的数字部分
-    numbers = re.findall(r'\d+', num_str)   #因为有+和K
-    return int(numbers[-1]) if numbers else 999
-# 定义一个自定义排序函数
-def custom_sort(s):
-    if "CCTV-4K" in s:
-        return 2  # 将包含 "4K" 的字符串排在后面
-    elif "CCTV-8K" in s:
-        return 3  # 将包含 "8K" 的字符串排在后面 
-    elif "(4K)" in s:
-        return 1  # 将包含 " (4K)" 的字符串排在后面
-    else:
-        return 0  # 其他字符串保持原顺序
-
-
-
-#读取whitelist,把高响应源从白名单中抽出加入merged_output。
-print(f"ADD whitelist_auto.txt")
-whitelist_auto_lines=read_txt_to_array('blacklist/whitelist_auto.txt') #
-for whitelist_line in whitelist_auto_lines:
-    if  "#genre#" not in whitelist_line and "," in whitelist_line and "://" in whitelist_line:
-        whitelist_parts = whitelist_line.split(",")
-        try:
-            response_time = float(whitelist_parts[0].replace("ms", ""))
-        except ValueError:
-            print(f"response_time转换失败: {whitelist_line}")
-            response_time = 60000  # 单位毫秒，转换失败给个60秒
-        if response_time < 2000:  #2s以内的高响应源
-            process_channel_line(",".join(whitelist_parts[1:]))
-
-
 # 合并所有对象中的行文本（去重，排序后拼接）
-# ["奥运频道,#genre#"] + sort_data(Olympics_2024_Paris_dictionary,set(correct_name_data(corrections_name,Olympics_2024_Paris_lines))) + ['\n'] + \
-version=datetime.now().strftime("%Y%m%d-%H-%M-%S")+",url"
-all_lines =  ["更新时间,#genre#"] +[version] + ['\n'] +\
-             ["央视频道,#genre#"] + sort_data(ys_dictionary,set(correct_name_data(corrections_name,ys_lines))) + ['\n'] + \
-             ["卫视频道,#genre#"] + sort_data(ws_dictionary,set(correct_name_data(corrections_name,ws_lines))) + ['\n'] + \
-             ["体育频道,#genre#"] + sort_data(ty_dictionary,set(correct_name_data(corrections_name,ty_lines))) + ['\n'] + \
-             ["电影频道,#genre#"] + sort_data(dy_dictionary,set(correct_name_data(corrections_name,dy_lines))) + ['\n'] + \
-             ["港澳台,#genre#"] + sort_data(gat_dictionary,set(correct_name_data(corrections_name,gat_lines))) + ['\n'] + \
-             ["湖南频道,#genre#"] + sorted(set(correct_name_data(corrections_name,hn_lines))) + ['\n'] + \
-
+version = datetime.now().strftime("%Y%m%d-%H-%M-%S") + ",url"
+all_lines = ["更新时间,#genre#"] + [version] + ['\n'] + \
+            ["专享源1️,#genre#"] + read_txt_to_array('主频道/♪专享源①.txt') + ['\n'] + \
+            ["专享源2️,#genre#"] + read_txt_to_array('主频道/♪专享源②.txt') + ['\n'] + \
+            ["专享央视,#genre#"] + read_txt_to_array('主频道/♪优质央视.txt') + ['\n'] + \
+            ["优质源,#genre#"] + read_txt_to_array('主频道/♪优质源.txt') + ['\n'] + \
+            ["央视频道,#genre#"] + sorted(set(ys_lines)) + ['\n'] + \
+            ["卫视频道,#genre#"] + sorted(set(ws_lines)) + ['\n'] + \
+            ["上海频道,#genre#"] + sorted(set(sh_lines)) + ['\n'] + \
+            ["体育频道,#genre#"] + sorted(set(ty_lines)) + ['\n'] + \
+            ["电影频道,#genre#"] + sorted(set(dy_lines)) + ['\n'] + \
+            ["电视剧频道,#genre#"] + sorted(set(dsj_lines)) + ['\n'] + \
+            ["港澳台,#genre#"] + sorted(set(gat_lines)) + ['\n'] + \
+            ["国际台,#genre#"] + sorted(set(gj_lines)) + ['\n'] + \
+            ["纪录片,#genre#"] + sorted(set(jlp_lines)) + ['\n'] + \
+            ["直播中国,#genre#"] + sorted(set(other_lines)) + ['\n']
 
 # 将合并后的文本写入文件
 output_file = "merged_output.txt"
@@ -331,46 +256,6 @@ try:
 
 except Exception as e:
     print(f"保存文件时发生错误：{e}")
-
-################# 添加生成m3u文件
-# 报时
-#print(f"time: {datetime.now().strftime("%Y%m%d_%H_%M_%S")}")
-
-channels_logos=read_txt_to_array('assets/logo.txt') #读入logo库
-def get_logo_by_channel_name(channel_name):
-    # 遍历数组查找频道名称
-    for line in channels_logos:
-        name, url = line.split(',')
-        if name == channel_name:
-            return url
-    return None
-
-output_text = '#EXTM3U x-tvg-url="https://live.fanmingming.com/e.xml,https://epg.112114.xyz/pp.xml.gz,https://assets.livednow.com/epg.xml"\n'
-
-with open(output_file, "r", encoding='utf-8') as file:
-    input_text = file.read()
-
-lines = input_text.strip().split("\n")
-group_name = ""
-for line in lines:
-    parts = line.split(",")
-    if len(parts) == 2 and "#genre#" in line:
-        group_name = parts[0]
-    elif len(parts) == 2:
-        channel_name = parts[0]
-        channel_url = parts[1]
-        logo_url=get_logo_by_channel_name(channel_name)
-        if logo_url is None:  #not found logo
-            output_text += f"#EXTINF:-1 group-title=\"{group_name}\",{channel_name}\n"
-            output_text += f"{channel_url}\n"
-        else:
-            output_text += f"#EXTINF:-1  tvg-name=\"{channel_name}\" tvg-logo=\"{logo_url}\"  group-title=\"{group_name}\",{channel_name}\n"
-            output_text += f"{channel_url}\n"
-
-with open("merged_output.m3u", "w", encoding='utf-8') as file:
-    file.write(output_text)
-
-print("merged_output.m3u文件已生成。")
 
 # 执行结束时间
 timeend = datetime.now()
@@ -396,9 +281,3 @@ other_lines_hj = len(other_lines)
 print(f"blacklist行数: {combined_blacklist_hj} ")
 print(f"merged_output.txt行数: {all_lines_hj} ")
 print(f"others_output.txt行数: {other_lines_hj} ")
-
-
-#备用1：http://tonkiang.us
-#备用2：https://www.zoomeye.hk
-#备用3：(BlackList检测对象)http,rtmp,p3p,rtp（rtsp，p2p）
-
